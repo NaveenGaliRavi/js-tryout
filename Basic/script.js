af5f9760cc9910c0064bd12e1075dd2393f53e83
Naveen <galinaveensai7@gmail.com>
2025-12-26 00:54:17 +0530

    initial commit JS basics

---
+++ Basic/script.js
@@ -0,0 +1,43 @@
+console.log("RaviNaveen");
+
+/*variable declarations
+let
+const
+var
+*/
+
+/*data types
+Objects
+Premitive Datatypes
+Number
+String
+Boolean
+symbol
+BigInt
+null
+undefined
+*/
+
+/*Operators
+Assignmnet operators (=)
+Arithmetic operators (+,-,/,*,%, **, ++, --)
+Augumented Assignment operators (==, -=, +=, *=, /=)
+logical operators (&&, ||, !)
+comparision operators (==, ===, !=, <=, >=, <,>)
+*/
+
+/*type coercion
+'5'+5 = 55 (string)
+'5'-1=4 (number)
+--string with + operator is always string.
+--string with -,*,/ operator is always number.
+*/
+
+/*type conversion
+Number() -'123f' converts as NaN
+String() -null, undefined converts to string 'null', 'undefined'
+toString() -null, undefined converts as undefined
+parseInt() -'123f' converts as 123 (number)
+parseFloat()
+Boolean() - 0, null, undefined converts to false
+*/
