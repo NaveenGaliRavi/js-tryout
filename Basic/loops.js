fca850abca6a45108d22c61476cf2dc73b60b356
Naveen <galinaveensai7@gmail.com>
2025-12-31 01:16:22 +0530

    loops adding

---
+++ Basic/loops.js
@@ -0,0 +1,67 @@
+//for loop
+
+// for(let i=0; i<50; i+=2) {
+//     // if(i%2===0){
+//     console.log(i);
+//     // }
+// }
+
+//prime or not in for 
+
+// let num=7;
+
+// let count=0;
+// for(let i=2 ; i<=num; i++) {
+//     if(num%i==0){
+//         count=count+1;
+//         console.log("inside");
+//     }
+// }
+// if(count>1) {
+//     console.log(num+" is Not Prime")
+// } else {
+//     console.log(num+" is Prime")
+// }
+// or 
+
+// let isPrime=true;
+// for(let i=2; i<num; i++){
+//     if(num%i==0){
+//         isPrime=false;
+//         break;
+//     }
+// }
+
+// if(isPrime) {
+//     console.log(num+" is Prime")
+// } else {
+//     console.log(num+" is Not Prime")
+// }
+
+//while loop
+
+// let i=1;
+
+// while(i < 20) {
+//     console.log(i);
+//     i++;
+// }
+
+
+//do-while loop
+//exit control loop
+
+let randomNum=Math.round(Math.random()*100);
+console.log(randomNum);
+let num;
+
+do {
+    num=prompt("enter number");
+    console.log("Enter num "+ num);
+
+    if(randomNum==num){
+        console.log("Gussed Correctly");
+    } else {
+        console.log("Try once more!")
+    }
+} while (randomNum!=num);
